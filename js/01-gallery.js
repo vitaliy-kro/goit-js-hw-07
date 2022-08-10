36f0f8b0d0a696787afb9296180d06ca31418719
vitaliy-kro <krochakv6@gmail.com>
2022-08-10 16:11:53 +0300

    finished 1 task, added prettier settings

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBoxRef = document.querySelector('.gallery');
+const createImages = galleryItems.map(createImgMarkup).join('');
+galleryBoxRef.insertAdjacentHTML('beforeend', createImages);
 
+galleryBoxRef.addEventListener('click', onImgClick);
+
+function createImgMarkup({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}
+
+function onImgClick(evt) {
+  const isContainsClass = evt.target.classList.contains('gallery__image');
+  if (!isContainsClass) return;
+  evt.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">`);
+  instance.show();
+  if (instance.show()) {
+    document.body.addEventListener('keydown', e => {
+      if (e.code === 'Escape' && instance) instance.close();
+    });
+  }
+}
 console.log(galleryItems);
